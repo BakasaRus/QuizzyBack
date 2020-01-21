f8461526421fbd78545e4c78d962f6ef38ea2647
Nick Andreev <bakasaru@list.ru>
2020-01-21 21:52:51 +0300

    Add controller for saving user's answers

---
+++ app/Http/Controllers/AttemptController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Question;
+use App\Test;
+use App\UserQuestion;
+use Illuminate\Http\Request;
+use Illuminate\Http\Response;
+use Illuminate\Support\Facades\Auth;
+
+class AttemptController extends Controller
+{
+    /**
+     * Save user's attempt
+     *
+     * @param  Request  $request
+     * @param  Test  $test
+     * @return Response
+     */
+    public function store(Request $request, Test $test)
+    {
+        $validated = $request->validate([
+            'answers' => 'required|array'
+        ]);
+
+        $valid_questions = $test->questions->pluck('id');
+        $answers = collect($validated['answers']);
+        $user = Auth::user();
+        $score = $answers->reduce(function ($total, $item) use ($valid_questions, $user) {
+            if (!$valid_questions->contains($item['id'])) {
+                return $total;
+            }
+
+            $question = Question::whereId($item['id'])->first();
+            $user_answer = $item['answer'];
+
+            if ($question->correct_answer == $user_answer) {
+                $user->answers()->attach($item['id'], [
+                    'answer' => $user_answer,
+                    'is_correct' => true
+                ]);
+                return $total + $question->points;
+            }
+        });
+
+        $test->users()->create([
+            'score' => $score
+        ]);
+
+        return response('', Response::HTTP_CREATED);
+    }
+}
